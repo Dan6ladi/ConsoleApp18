c984c876b97343ac018ee39edc10e364c6b58225
Dan6ladi <dan6ladi@gmail.com>
2022-08-05 11:55:46 +0100

    Add project files.

---
+++ ConsoleApp18/IStudyVisaAdmissionRules.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp18
+{
+    internal interface IStudyVisaAdmissionRules
+    {
+        public int transcript { get; set; }
+
+        public double Discipline(double grade , double studyGap);
+
+        public bool PassPort(string PassPortType);
+
+    }
+}
---
+++ ConsoleApp18/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace ConsoleApp18
+{
+    internal interface IStudyVisaAdmissionRules
+    {
+        public int transcript { get; set; }
+
+        public double Discipline(double grade, double studyGap);
+
+        public bool PassPort(string PassPortType);
+
+    }
+    internal class Canada : IStudyVisaAdmissionRules
+    {
+        public int transcript { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        static void Main(string[] args)
+        {
+            Canada canada = new Canada();
+            Console.WriteLine("welcome to the canadian Embassy. ibrahim");
+            string name = Console.ReadLine();
+            Console.WriteLine("Congratulations you just got Admitted!");
+        }
+
+        public bool PassPort(string PassPortType)
+        {
+            return true;
+        }
+
+        public double Discipline(double grade, double studyGap)
+        {
+            double Discipline = grade * studyGap;
+            return Discipline;
+
+        }
+    }
+}
